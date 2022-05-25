99d560d52f35e91492e22f00bc5b8e27b26b28e1
SandeepMS07 <sandeepms746@gmail.com>
2022-05-25 13:27:46 +0530

    class in typescript

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
 var message = "Welcome back";
 console.log(message);
@@ -39,8 +54,8 @@ randomValue = true;
 randomValue = "Sandy";
 var myVariable = 10;
 console.log(myVariable.name);
-myVariable();
-myVariable.toUpperCase();
+// myVariable();
+// myVariable.toUpperCase();
 // unknown
 var naem = 10;
 function hasname(obj) {
@@ -49,4 +64,106 @@ function hasname(obj) {
 if (hasname(naem)) {
     console.log(naem.name);
 }
-naem.toUpperCase();
+// (naem as string).toUpperCase();
+//multitype
+var a;
+a = 10;
+a = true;
+var b = 20;
+var multitype;
+multitype = 20;
+multitype = true;
+var anyType;
+anyType: 20;
+/**
+ * functions
+ *
+ */
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return num1 + num2;
+}
+add(5, 10);
+// optional parameter => (?) symbol for optional
+function add1(num1, num2) {
+    if (num2) {
+        console.log(num1 + num2);
+    }
+    else {
+        console.log(num1);
+    }
+    return num1 + num2;
+}
+add1(5);
+// default paramaeter
+function add2(num1, num2) {
+    if (num2 === void 0) { num2 = 10; }
+    if (num2) {
+        console.log(num1 + num2);
+    }
+    else {
+        console.log(num1);
+    }
+    return num1 + num2;
+}
+add2(10, 5);
+add2(1);
+/**
+ * interface in typescript
+ */
+//example
+function fullname(person) {
+    console.log("".concat(person.firstname, " ").concat(person.lastname));
+}
+var p = {
+    firstname: "Sandeep",
+    lastname: "M S"
+};
+fullname(p);
+function fullname1(person) {
+    console.log("".concat(person.firstname, " ").concat(person.lastname));
+}
+var p2 = {
+    firstname: "Sandeep",
+    lastname: "M S"
+};
+fullname1(p2);
+function fullname2(person) {
+    console.log("".concat(person.firstname, " ").concat(person.lastname));
+}
+var p1 = {
+    firstname: "Sandeep"
+};
+fullname2(p1);
+/**
+ *
+ * class
+ */
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good Morning ".concat(this.employeeName));
+    };
+    return Employee;
+}());
+var emp1 = new Employee("Sandeep");
+console.log(emp1.employeeName);
+emp1.greet();
+//
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this;
+    }
+    Manager.prototype.delegateWork = function () {
+        console.log("Manager delegating tasks");
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager("Bruce");
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
+console.log(emp1.employeeName);
--- main.ts
+++ main.ts
@@ -48,8 +48,8 @@ randomValue = "Sandy";
 
 let myVariable: any = 10;
 console.log(myVariable.name);
-myVariable();
-myVariable.toUpperCase();
+// myVariable();
+// myVariable.toUpperCase();
 
 // unknown
 let naem: unknown = 10;
@@ -61,4 +61,130 @@ function hasname(obj: any): obj is { name: string } {
 if (hasname(naem)) {
   console.log(naem.name);
 }
-(naem as string).toUpperCase();
+// (naem as string).toUpperCase();
+
+//multitype
+let a;
+a = 10;
+a = true;
+
+let b = 20;
+
+let multitype: number | boolean;
+multitype = 20;
+multitype = true;
+
+let anyType: any;
+anyType: 20;
+
+/**
+ * functions
+ *
+ */
+function add(num1: number, num2: number) {
+  console.log(num1 + num2);
+
+  return num1 + num2;
+}
+add(5, 10);
+
+// optional parameter => (?) symbol for optional
+function add1(num1: number, num2?: number): number {
+  if (num2) {
+    console.log(num1 + num2);
+  } else {
+    console.log(num1);
+  }
+  return num1 + num2;
+}
+add1(5);
+
+// default paramaeter
+function add2(num1: number, num2: number = 10): number {
+  if (num2) {
+    console.log(num1 + num2);
+  } else {
+    console.log(num1);
+  }
+  return num1 + num2;
+}
+add2(10, 5);
+add2(1);
+
+/**
+ * interface in typescript
+ */
+
+//example
+function fullname(person: { firstname: string; lastname: string }) {
+  console.log(`${person.firstname} ${person.lastname}`);
+}
+let p = {
+  firstname: "Sandeep",
+  lastname: "M S",
+};
+fullname(p);
+
+//interface
+interface person1 {
+  firstname: string;
+  lastname: string;
+}
+function fullname1(person: person1) {
+  console.log(`${person.firstname} ${person.lastname}`);
+}
+let p2 = {
+  firstname: "Sandeep",
+  lastname: "M S",
+};
+fullname1(p2);
+
+// optional
+interface person2 {
+  firstname: string;
+  lastname?: string;
+}
+function fullname2(person: person2) {
+  console.log(`${person.firstname} ${person.lastname}`);
+}
+let p1 = {
+  firstname: "Sandeep",
+};
+fullname2(p1);
+
+/**
+ *
+ * class
+ */
+
+class Employee {
+  employeeName: string;
+
+  constructor(name: string) {
+    this.employeeName = name;
+  }
+
+  greet() {
+    console.log(`Good Morning ${this.employeeName}`);
+  }
+}
+
+let emp1 = new Employee("Sandeep");
+console.log(emp1.employeeName);
+emp1.greet();
+
+//
+class Manager extends Employee {
+  constructor(managerName: string) {
+    super(managerName);
+  }
+  delegateWork() {
+    console.log(`Manager delegating tasks`);
+  }
+}
+
+let m1 = new Manager("Bruce");
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
+console.log(emp1.employeeName);
